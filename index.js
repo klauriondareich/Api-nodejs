529e666d0507fc2c29fdfbcbfca8fce51a4e4a2e
laurion <klauriondareich@gmail.com>
2022-04-06 17:03:21 +0200

    Node project configured: express, mongodb, nodemon and mongoose installed

---
+++ index.js
@@ -0,0 +1,10 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) =>{
+    res.send(`Your server is running in port=${port}`)
+});
+
+app.listen(port, () => console.log("server is running"));
