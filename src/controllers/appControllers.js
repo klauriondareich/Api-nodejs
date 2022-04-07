19d8a5a4e959bc4b5bdbd514afe2185e2092b336
laurion <klauriondareich@gmail.com>
2022-04-07 11:11:32 +0200

    createNewUser func added in controller

---
+++ src/controllers/appControllers.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+import { UserSchema } from '../models/appModels';
+
+const User = mongoose.model('user', UserSchema);
+
+export const createNewUser = (req, res) =>{
+
+    let newUser = new UserSchema(req.body);
+
+    newUser.save((err, user) =>{
+        if (err){
+            res.send(err)
+        }
+        res.json(user)
+    });
+}
