909be717be93ca4de01bd1a5dc8606eea09d20e2
laurion <klauriondareich@gmail.com>
2022-04-07 11:04:07 +0200

    Users model added

---
+++ src/models/appModels.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+export const UserSchema = new Schema({
+
+    id:{
+        type: Number,
+        required: "a user must have an id"
+    },
+    pseudo:{
+        type: String,
+        required: "a user must have a pseudo"
+    },
+    email:{
+        type: String,
+        required: "a user must have an email address"
+    },
+    password:{
+        type: String,
+        required: "a user must have a password"
+    },
+    isAdmin:{
+        type: Boolean,
+        required: "user role must be defined"
+    },
+    
+})
