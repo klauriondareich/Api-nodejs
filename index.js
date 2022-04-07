72fc7d588fb8ce70a773a83ad925c30c9922a6ab
laurion <klauriondareich@gmail.com>
2022-04-07 10:54:11 +0200

    mongo db connection and body parser configs added

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
 import routes from './src/routes/appRoutes.js';
 
 const app = express();
 const PORT = 3000;
+
+// Database connection
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb+srv://root:<password>@cluster0.mflgi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
+
+// Body Parser config
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 routes(app);
 
