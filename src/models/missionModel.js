49388729a50527cf06245bbf5b4e09260584b432
laurion <klauriondareich@gmail.com>
2022-04-10 00:52:14 +0200

    missionModel added

---
+++ src/models/missionModel.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+export const MissionSchema = new Schema({
+
+    country:{
+        type: String,
+        required: "a mission must have a country"
+    },
+    start_date:{
+        type: Date,
+        required: "a mission must have a start_date"
+    },
+    end_date:{
+        type: Date,
+        required: "a mission must have an end date"
+    },
+    rovers:{
+        type: Array,
+        required: "a mission must have rovers"
+    },
+    created_at:{
+        type: Date,
+        default: Date.now
+    }
+})
